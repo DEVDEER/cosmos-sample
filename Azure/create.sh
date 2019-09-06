f1fd6a6713ff398e42bbd1db3d19b27a5f214f79
alexander.schmidt <alexander.schmidt@devdeer.com>
2019-09-06 14:19:20 +0200

    Finished creation script

--- Azure/create.sh
+++ Azure/create.sh
@@ -1,20 +1,48 @@
-# bin/bash
-# Change this values!
-RESOURCEGROUP_NAME='TODO'
-LOCATION='TODO'
-SQL_SERVER_NAME='TODO'
-SQL_ADMIN_USERNAME='TODO'
-SQL_ADMIN_PASSWORD='TODO'
-STORAGE_ACCOUNT_NAME='TODO'
-APPINSIGHTS_NAME='TODO'
-SERVICEPLAN_NAME='TODO'
-SERVICEPLAN_SKU='TODO'
-FUNCTIONAPP_NAME='TODO'
-COSMOSDB_NAME='TODO'
+#! /bin/bash
 
-# don't touch from here!
+# Replace all '?' with your values!
+RESOURCEGROUP_NAME='?'
+LOCATION='?'
+SQL_SERVER_NAME='?'
+SQL_ADMIN_USERNAME='?'
+SQL_ADMIN_PASSWORD='?'
+STORAGE_ACCOUNT_NAME='?'
+APPINSIGHTS_NAME='?'
+SERVICEPLAN_NAME='?'
+SERVICEPLAN_SKU='?'
+FUNCTIONAPP_NAME='?'
+COSMOSDB_NAME='?'
+
+# Don't touch from here!
+
 GREY='\033[0;37m'
 GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+function cleanUp() {
+    echo -e "${GREY}Cleaning up environment..."
+    unset COSMOSDB_KEY
+    unset COSMOS_CONNECTION
+    unset RESOURCEGROUP_NAME
+    unset LOCATION
+    unset SQL_SERVER_NAME
+    unset SQL_ADMIN_USERNAME
+    unset SQL_ADMIN_PASSWORD
+    unset STORAGE_ACCOUNT_NAME
+    unset APPINSIGHTS_NAME
+    unset SERVICEPLAN_NAME
+    unset SERVICEPLAN_SKU
+    unset FUNCTIONAPP_NAME
+    unset COSMOSDB_NAME
+    echo -e "${GREEN}Finished."
+}
+
+function onError {
+    cleanUp
+    echo -e "${GREY}An error occured. Exiting script."
+}
+
+trap onError ERR
 
 echo -e "${GREY}Creating resource group $RESOURCEGROUP_NAME..."
 az group create -n $RESOURCEGROUP_NAME --location $LOCATION --output none
@@ -27,7 +55,7 @@ az sql db create --resource-group $RESOURCEGROUP_NAME --server $SQL_SERVER_NAME 
 echo -e "${GREEN}Finished."
 
 echo -e "${GREY}Creating storage account $STORAGE_ACCOUNT_NAME..."
-az storage account create --resource-group $RESOURCEGROUP_NAME -n $STORAGE_ACCOUNT_NAME --output none
+az storage account create --resource-group $RESOURCEGROUP_NAME -n $STORAGE_ACCOUNT_NAME --sku Standard_LRS --kind StorageV2 --https-only true --access-tier hot --output none
 echo -e "${GREEN}Finished."
 
 echo -e "${GREY}Creating Application Insights $APPINSIGHTS_NAME..."
@@ -44,9 +72,11 @@ echo -e "${GREEN}Finished."
 
 echo -e "${GREY}Creating Function App $FUNCTIONAPP_NAME with App Service Plan $SERVICEPLAN_NAME..."
 az appservice plan create -n $SERVICEPLAN_NAME --resource-group $RESOURCEGROUP_NAME --sku $SERVICEPLAN_SKU  --output none
-az functionapp create --resource-group $RESOURCE_GROUP -n $FUNCTIONAPP_NAME --storage-account $STORAGE_ACCOUNT_NAME --runtime node --app-insights $APPINSIGHTS_NAME --output none
-az functionapp config appsettings set --name $FUNCTIONAPP_NAME --resource-group $RESOURCEGROUP_NAME --settings "CosmosConnectionString="
-az functionapp config appsettings set --name $FUNCTIONAPP_NAME --resource-group $RESOURCEGROUP_NAME --settings "SQL_SERVER_ADDRESS=$SQL_SERVER_NAME.database.windows.net"
-az functionapp config appsettings set --name $FUNCTIONAPP_NAME --resource-group $RESOURCEGROUP_NAME --settings "SQL_SERVER_USER=$SQL_ADMIN_USERNAME"
-az functionapp config appsettings set --name $FUNCTIONAPP_NAME --resource-group $RESOURCEGROUP_NAME --settings "SQL_SERVER_PASSWORD=$SQL_ADMIN_PASSWORD"
+az functionapp create --resource-group $RESOURCEGROUP_NAME -n $FUNCTIONAPP_NAME --storage-account $STORAGE_ACCOUNT_NAME --runtime node --app-insights $APPINSIGHTS_NAME  --consumption-plan-location $LOCATION --output none
+az functionapp config appsettings set --name $FUNCTIONAPP_NAME --resource-group $RESOURCEGROUP_NAME --settings "CosmosConnectionString=" --output none
+az functionapp config appsettings set --name $FUNCTIONAPP_NAME --resource-group $RESOURCEGROUP_NAME --settings "SQL_SERVER_ADDRESS=$SQL_SERVER_NAME.database.windows.net" --output none
+az functionapp config appsettings set --name $FUNCTIONAPP_NAME --resource-group $RESOURCEGROUP_NAME --settings "SQL_SERVER_USER=$SQL_ADMIN_USERNAME" --output none
+az functionapp config appsettings set --name $FUNCTIONAPP_NAME --resource-group $RESOURCEGROUP_NAME --settings "SQL_SERVER_PASSWORD=$SQL_ADMIN_PASSWORD" --output none
 echo -e "${GREEN}Finished."
+
+cleanUp
